0463857f68623597f8953be32efef289af1d907f
Bruno Costa <bruno.ncosta9@gmail.com>
2020-04-21 22:57:07 +0100

    Update Assets

--- Assets/js/data/scenes.js
+++ Assets/js/data/scenes.js
@@ -3,77 +3,246 @@
 function scenes(game){
 
    return {
-      sky: {
-         name: "sky",
+      layer_1: {
+         name: "layer_1",
          draw: {
             pattern: true,
             fillRect: true,
             image: false,
             static: {
-               1: "Assets/img/bg/sky.png"
+               1: "Assets/img/bg/Layer_0010_1.png"
             }
          },
          position:{
             x: 0,
             y: 0,
             width: game._canvas.width(),
-            height: game._canvas.height()
+            height: 793
          },
          translate: {
             flag: true,
             x: 0,
-            y: 0
+            y: game._canvas.height() - 793
          }
       },
-      floor: {
-         name: "floor",
+      layer_2: {
+         name: "layer_2",
          draw: {
             pattern: true,
             fillRect: true,
             image: false,
             static: {
-               1: "Assets/img/bg/floor.png"
+               1: "Assets/img/bg/Layer_0009_2.png"
             }
          },
          position:{
             x: 0,
             y: 0,
             width: game._canvas.width(),
-            height: 64
+            height: 793
          },
          translate: {
             flag: true,
             x: 0,
-            y: game._canvas.height() - 64
+            y: game._canvas.height() - 793
          }
       },
-      box: {
-         name: "box",
+      layer_3: {
+         name: "layer_3",
          draw: {
             pattern: true,
             fillRect: true,
             image: false,
             static: {
-               1: "Assets/img/bg/floor.png"
+               1: "Assets/img/bg/Layer_0008_3.png"
             }
          },
          position:{
             x: 0,
             y: 0,
             width: game._canvas.width(),
-            height: 64
+            height: 793
          },
          translate: {
             flag: true,
             x: 0,
-            y: game._canvas.height() - 64
+            y: game._canvas.height() - 793
+         }
+      },
+      layer_4: {
+         name: "layer_4",
+         draw: {
+            pattern: true,
+            fillRect: true,
+            image: false,
+            static: {
+               1: "Assets/img/bg/Layer_0007_Lights.png"
+            }
          },
-         collision: {
+         position:{
+            x: 0,
+            y: 0,
+            width: game._canvas.width(),
+            height: 793
+         },
+         translate: {
             flag: true,
-            top: true,
-            right: true,
-            bottom: true,
-            left: true
+            x: 0,
+            y: game._canvas.height() - 793
+         }
+      },
+      layer_5: {
+         name: "layer_5",
+         draw: {
+            pattern: true,
+            fillRect: true,
+            image: false,
+            static: {
+               1: "Assets/img/bg/Layer_0006_4.png"
+            }
+         },
+         position:{
+            x: 0,
+            y: 0,
+            width: game._canvas.width(),
+            height: 793
+         },
+         translate: {
+            flag: true,
+            x: 0,
+            y: game._canvas.height() - 793
+         }
+      },
+      layer_6: {
+         name: "layer_6",
+         draw: {
+            pattern: true,
+            fillRect: true,
+            image: false,
+            static: {
+               1: "Assets/img/bg/Layer_0005_5.png"
+            }
+         },
+         position:{
+            x: 0,
+            y: 0,
+            width: game._canvas.width(),
+            height: 793
+         },
+         translate: {
+            flag: true,
+            x: 0,
+            y: game._canvas.height() - 793
+         }
+      },
+      layer_7: {
+         name: "layer_7",
+         draw: {
+            pattern: true,
+            fillRect: true,
+            image: false,
+            static: {
+               1: "Assets/img/bg/Layer_0004_Lights.png"
+            }
+         },
+         position:{
+            x: 0,
+            y: 0,
+            width: game._canvas.width(),
+            height: 793
+         },
+         translate: {
+            flag: true,
+            x: 0,
+            y: game._canvas.height() - 793
+         }
+      },
+      layer_8: {
+         name: "layer_8",
+         draw: {
+            pattern: true,
+            fillRect: true,
+            image: false,
+            static: {
+               1: "Assets/img/bg/Layer_0003_6.png"
+            }
+         },
+         position:{
+            x: 0,
+            y: 0,
+            width: game._canvas.width(),
+            height: 793
+         },
+         translate: {
+            flag: true,
+            x: 0,
+            y: game._canvas.height() - 793
+         }
+      },
+      layer_9: {
+         name: "layer_9",
+         draw: {
+            pattern: true,
+            fillRect: true,
+            image: false,
+            static: {
+               1: "Assets/img/bg/Layer_0002_7.png"
+            }
+         },
+         position:{
+            x: 0,
+            y: 0,
+            width: game._canvas.width(),
+            height: 793
+         },
+         translate: {
+            flag: true,
+            x: 0,
+            y: game._canvas.height() - 793
+         }
+      },
+      layer_10: {
+         name: "layer_10",
+         draw: {
+            pattern: true,
+            fillRect: true,
+            image: false,
+            static: {
+               1: "Assets/img/bg/Layer_0001_8.png"
+            }
+         },
+         position:{
+            x: 0,
+            y: 0,
+            width: game._canvas.width(),
+            height: 793
+         },
+         translate: {
+            flag: true,
+            x: 0,
+            y: game._canvas.height() - 793
+         }
+      },
+      layer_11: {
+         name: "layer_11",
+         draw: {
+            pattern: true,
+            fillRect: true,
+            image: false,
+            static: {
+               1: "Assets/img/bg/Layer_0000_9.png"
+            }
+         },
+         position:{
+            x: 0,
+            y: 0,
+            width: game._canvas.width(),
+            height: 793
+         },
+         translate: {
+            flag: true,
+            x: 0,
+            y: game._canvas.height() - 793
          }
       }
    };
--- Assets/js/main.js
+++ Assets/js/main.js
@@ -9,8 +9,17 @@ game.init();
 const _scenes = scenes(game);
 
 // "Pre"Load Images
-game.load("images", _scenes.sky.draw.static);
-game.load("images", _scenes.floor.draw.static);
+game.load("images", _scenes.layer_1.draw.static);
+game.load("images", _scenes.layer_2.draw.static);
+game.load("images", _scenes.layer_3.draw.static);
+game.load("images", _scenes.layer_4.draw.static);
+game.load("images", _scenes.layer_5.draw.static);
+game.load("images", _scenes.layer_6.draw.static);
+game.load("images", _scenes.layer_7.draw.static);
+game.load("images", _scenes.layer_8.draw.static);
+game.load("images", _scenes.layer_9.draw.static);
+game.load("images", _scenes.layer_10.draw.static);
+game.load("images", _scenes.layer_11.draw.static);
 
 // Load all the Data
 game.load("data", _scenes);
